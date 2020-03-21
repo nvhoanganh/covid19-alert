373946929b4cd50e79210e7ff84f70dcbffe9131
anthony <nvhoanganh1909@gmail.com>
2020-03-21 22:23:06 +1100

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TiCogOutline, TiMap, TiThListOutline } from 'react-icons/ti';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import MyMapComponent from './Map';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<body className="flex flex-col min-h-screen">
+			<header className="text-center p-5 border-b-4">Map</header>
+			<main className="flex-grow">
+				<div className="p-8">
+					<MyMapComponent />
+				</div>
+			</main>
+			<footer>
+				<div className="flex justify-center border-t-2">
+					<a
+						href="https://timkoech.co.ke"
+						className="text-gray-702 hover:text-gray-800 text-3xl px-8 bg-gray-300 py-2"
+					>
+						<TiMap></TiMap>
+					</a>
+					<a
+						href="https://timkoech.co.ke"
+						className="text-gray-702 hover:text-gray-800 text-3xl px-8 py-2"
+					>
+						<TiThListOutline></TiThListOutline>
+					</a>
+					<a
+						href="https://timkoech.co.ke"
+						className="text-gray-702 hover:text-gray-800 text-3xl px-8 py-2"
+					>
+						<TiCogOutline></TiCogOutline>
+					</a>
+				</div>
+			</footer>
+		</body>
+	);
 }
-
 export default App;
